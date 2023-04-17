598b5efe51a5035ef2f453d2ea6923415f4bfcaa
jose <jar2333@columbia.edu>
2023-04-17 03:08:23 +0000

    made a UTerm typeclass

--- src/Term.hs
+++ src/Term.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Term
     ( Term(..)
     , Var
@@ -10,21 +12,25 @@ import Data.Map as Map ( lookup, Map )
 import Data.Char ( toUpper, toLower )
 
 type Var = Int
+type Bind = Map.Map String Var
+
+class UTerm t where
+    pretty :: t -> String 
+    substID :: t -> Bind -> Term
 
 data Term = ID String | Var Var | Symbol String | Bool Bool | Nil | Pair Term Term deriving (Eq, Show)
 
-pretty :: Term -> String
-pretty (Pair t1 t2) = "(" ++ pretty t1 ++ ", " ++ pretty t2 ++ ")"
-pretty (ID i) = map toUpper i
-pretty (Var v) = show v
-pretty (Symbol s) = s
-pretty (Bool b) = map toLower $ show b
-pretty Nil = "()"
+instance UTerm Term where
+    pretty :: Term -> String
+    pretty (Pair t1 t2) = "(" ++ pretty t1 ++ ", " ++ pretty t2 ++ ")"
+    pretty (ID i) = map toUpper i
+    pretty (Var v) = show v
+    pretty (Symbol s) = s
+    pretty (Bool b) = map toLower $ show b
+    pretty Nil = "()"
 
-type Bind  = Map.Map String Var
-
-substID :: Term -> Bind -> Term
-substID (ID i) b = maybe (error $ i ++ " NOT FOUND") Var (Map.lookup i b)
-substID (Pair t1 t2) b = Pair (substID t1 b) (substID t2 b)
-substID x _ = x
+    substID :: Term -> Bind -> Term
+    substID (ID i) b = maybe (error $ i ++ " NOT FOUND") Var (Map.lookup i b)
+    substID (Pair t1 t2) b = Pair (substID t1 b) (substID t2 b)
+    substID x _ = x
 
