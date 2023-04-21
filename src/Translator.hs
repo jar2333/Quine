dc5ac72bd77e9042ca8824d9efc52c796965d6fc
yiming-fang <yf2484@columbia.edu>
2023-04-21 01:44:07 -0400

    add Translator for LambdaTerm

--- src/Translator.hs
+++ src/Translator.hs
@@ -1,16 +1,17 @@
-module Translator 
-    ( translateProgram
-    , translateStatement
-    ) where
-
+module Translator (
+    translateProgram,
+    translateStatement,
+) where
 
 import Control.Monad.IO.Class
 
-import AST
-import UTerm
+import AST as A
+import Data.Maybe (fromJust)
 import Kanren
+import LambdaTerm as L
 import Print
-import LambdaTerm
+import Semant
+import UTerm
 
 type Statement t = KanrenT t IO ()
 
@@ -19,33 +20,43 @@ type Statement t = KanrenT t IO ()
 ---
 
 rule :: String -> [String] -> Kanren.Goal LambdaTerm -> Translator.Statement LambdaTerm
-rule name idents g = do defineRelation name idents g ; liftIO $ printRelation name idents
+rule name idents g = do defineRelation name idents g; liftIO $ printRelation name idents
 
 query :: Maybe Int -> [String] -> Kanren.Goal LambdaTerm -> Translator.Statement LambdaTerm
-query Nothing  idents g = do stream <- runAll idents g    ; liftIO $ printStream stream -- Replace with run?
-query (Just i) idents g = do stream <- runMany i idents g ; liftIO $ printStream stream
-
+query Nothing idents g = do stream <- runAll idents g; liftIO $ printStream stream -- Replace with run?
+query (Just i) idents g = do stream <- runMany i idents g; liftIO $ printStream stream
 
 ---
 -- Translate AST to executable statements.
 ---
 
-translateProgram :: [AST.Statement] -> [Translator.Statement LambdaTerm]
+translateProgram :: [A.Statement] -> [Translator.Statement LambdaTerm]
 translateProgram = map translateStatement
 
-translateStatement :: AST.Statement -> Translator.Statement LambdaTerm
+translateStatement :: A.Statement -> Translator.Statement LambdaTerm
 translateStatement (Rule name idents goal) = rule name idents (translateGoal goal)
-translateStatement (Query i idents goal)   = query i idents (translateGoal goal) 
+translateStatement (Query i idents goal) = query i idents (translateGoal goal)
 
-translateGoal :: AST.Goal -> Kanren.Goal LambdaTerm
-translateGoal (Disj g1 g2)         = disj (translateGoal g1) (translateGoal g2)
-translateGoal (Conj g1 g2)         = conj (translateGoal g1) (translateGoal g2)
-translateGoal (Fresh uvars g)      = fresh uvars (translateGoal g)
-translateGoal (Equal t1 t2)        = translateTerm t1 === translateTerm t2
+translateGoal :: A.Goal -> Kanren.Goal LambdaTerm
+translateGoal (Disj g1 g2) = disj (translateGoal g1) (translateGoal g2)
+translateGoal (Conj g1 g2) = conj (translateGoal g1) (translateGoal g2)
+translateGoal (Fresh uvars g) = fresh uvars (translateGoal g)
+translateGoal (Equal t1 t2) = translateTerm t1 === translateTerm t2
 translateGoal (Relation name args) = callRelation name (map getArg args)
-    where getArg (Param u) = uvar u
-          getArg (Term t)  = translateTerm t
+  where
+    getArg (Param u) = uvar u
+    getArg (Term t) = translateTerm t
 
-translateTerm :: AST.Term -> LambdaTerm
-translateTerm (UVar u) = uvar u
-translateTerm _ = error "Not implemented." 
+translateTerm :: A.Term -> L.LambdaTerm
+translateTerm term =
+    case term of
+        A.UVar u -> uvar u
+        A.Var v -> L.LVar v ty
+        A.Abs b e -> L.Abs b (translateTerm e) ty
+        A.App f x -> L.App (translateTerm f) (translateTerm x) ty
+        A.Let v e1 e2 -> L.Let v (translateTerm e1) (translateTerm e2) ty
+        A.Pair l r -> L.Pair (translateTerm l) (translateTerm r) ty
+        A.Fst e -> L.Fst (translateTerm e) ty
+        A.Snd e -> L.Snd (translateTerm e) ty
+  where
+    ty = fromJust $ typeof [] term
