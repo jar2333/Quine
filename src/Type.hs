75a908d0e4b0274542ce75972f0ab1002f184f51
yiming-fang <yf2484@columbia.edu>
2023-04-20 23:49:11 -0400

    add Type.hs

---
+++ src/Type.hs
@@ -0,0 +1,21 @@
+module Type
+  ( Type(..)
+  ) where
+
+data Type = UserDefined String
+          | Arrow Type Type
+          | Prod Type Type
+          | Sum Type Type
+          | List Type
+          | Int
+          | Bool
+        deriving (Eq)
+
+instance Show Type where
+  show (UserDefined s) = s
+  show Int = "Int"
+  show Bool = "Bool"
+  show (Arrow t1 t2) = show t1 ++ " -> " ++ show t2
+  show (Prod t1 t2) = show t1 ++ " * " ++ show t2
+  show (Sum t1 t2) = show t1 ++ " + " ++ show t2
+  show (List t) = "[" ++ show t ++ "]" 
