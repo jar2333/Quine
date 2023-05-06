d967956e92e9cf234715c5467684e80af420bd48
jose <jar2333@columbia.edu>
2023-05-06 01:33:44 +0000

    lambdaterm file refactored, implemented substitute

--- src/LambdaTerm.hs
+++ src/LambdaTerm.hs
@@ -14,8 +14,10 @@ module LambdaTerm (
 import Control.Monad
 import Control.Monad.Logic
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.Maybe (fromMaybe)
 import qualified Data.Maybe as Maybe
+
 import Type
 import UTerm (USubst, UTerm (..))
 
@@ -74,27 +76,35 @@ instance UTerm LambdaTerm where
     -- subst N x M = M[x := N]
     -- Substitute all instances of a uvar x with N in M
     substitute :: LambdaTerm -> String -> LambdaTerm -> LambdaTerm
-    substitute _ _ _ = error "not implemented"
+    substitute n x u@(UVar y) | y == x    = n
+                              | otherwise = u
+    substitute n x (Abs b t ty)      = Abs b (substitute n x t) ty
+    substitute n x (App t1 t2 ty)    = App (substitute n x t1) (substitute n x t2) ty
+    substitute n x (Let y t1 t2 ty)  = Let y (substitute n x t1) (substitute n x t2) ty
+    substitute n x (Pair t1 t2 ty)   = Pair (substitute n x t1) (substitute n x t2) ty
+    substitute n x (Fst t ty)        = Fst (substitute n x t) ty
+    substitute n x (Snd t ty)        = Snd (substitute n x t) ty
+    substitute _ _ m                 = m
 
     -- Find a stream of substitutions that can unify the two terms given a base substitution.
     unify :: LambdaTerm -> LambdaTerm -> Subst -> Logic Subst
     unify _ _ _ = mzero
 
-    -- Use a substitution to replace each uvar subterm with the corresponding term in the substitution
-    replace :: LambdaTerm -> Subst -> LambdaTerm
-    replace (ID v) subst = fromMaybe (UVar "_") (getTerm subst v)
-    replace (Abs bin term t) subst = Abs bin (replace term subst) t
-    replace (App t1 t2 t) subst = App (replace t1 subst) (replace t2 subst) t
-    replace (Let v t1 t2 t) subst = Let v (replace t1 subst) (replace t2 subst) t
-    replace (Pair t1 t2 t) subst = Pair (replace t1 subst) (replace t2 subst) t
-    replace (Fst e t) subst = Fst (replace e subst) t
-    replace (Snd e t) subst = Snd (replace e subst) t
-    replace x _ = x
-
     -- Find term corredponding to an identifier term, return itself on failure or if not an identifier term
     find :: LambdaTerm -> Subst -> LambdaTerm
     find (ID u) s = Maybe.fromMaybe (ID u) (Map.lookup u s)
     find t _ = t
+
+    -- Use a substitution to replace each uvar subterm with the corresponding term in the substitution
+    replace :: LambdaTerm -> Subst -> LambdaTerm
+    replace (ID v) s = fromMaybe (UVar "_") (getTerm s v)
+    replace (Abs bin term t) s = Abs bin (replace term s) t
+    replace (App t1 t2 t) s = App (replace t1 s) (replace t2 s) t
+    replace (Let v t1 t2 t) s = Let v (replace t1 s) (replace t2 s) t
+    replace (Pair t1 t2 t) s = Pair (replace t1 s) (replace t2 s) t
+    replace (Fst e t) s = Fst (replace e s) t
+    replace (Snd e t) s = Snd (replace e s) t
+    replace x _ = x
 
     -- Wrap the given string as a uvar term.
     uvar :: String -> LambdaTerm
@@ -103,3 +113,108 @@ instance UTerm LambdaTerm where
     -- Wrap the given ID as a ID term.
     ident :: Int -> LambdaTerm
     ident = ID
+
+---
+-- Unification
+---
+
+
+
+
+---
+-- Lambda Calculus Interpreter
+---
+
+-- | Extract the type information from the term
+extract :: LambdaTerm -> Type
+extract (Var _ t) = t
+extract (Abs _ _ t) = t
+extract (App _ _ t) = t
+extract (Let _ _ _ t) = t
+extract (Pair _ _ t) = t
+extract (Fst _ t) = t
+extract (Snd _ t) = t
+extract _ = Hole
+
+-- | Return the free variables in a lambda term
+fv :: LambdaTerm -> Set.Set LambdaVar
+fv (Var varid _) = Set.singleton varid
+fv (Abs bin body _) = Set.difference (fv body) (Set.singleton $ fst bin)
+fv (App fun arg _) = Set.union (fv fun) (fv arg)
+fv (Let lv _ body _) = Set.difference (fv body) (Set.singleton lv)
+fv (Pair l r _) = Set.union (fv l) (fv r)
+fv (Fst term _) = fv term
+fv (Snd term _) = fv term
+fv _ = Set.empty
+
+{- | Return a "fresh" name not already in the set.
+ Tries x' then x'', etc.
+-}
+freshVar :: Set.Set LambdaVar -> LambdaVar -> LambdaVar
+freshVar s = fresh'
+  where
+    fresh' n | n `Set.notMember` s = n
+    fresh' n = fresh' $ n ++ "'"
+
+-- subst N x M = M[x := N]
+-- Substitute all instances of a lambda var x with N in M
+subst :: LambdaTerm -> String -> LambdaTerm -> LambdaTerm
+subst n x v@(Var y _) = if y == x then n else v
+subst n x (App fun arg t) = App (subst n x fun) (subst n x arg) t
+subst n x a@(Abs bin@(y, yt) body t)
+    | y == x = a
+    | y `Set.notMember` fv n = Abs bin (subst n x body) t
+    | otherwise = Abs bin' (subst n x body') t
+  where
+    body' = subst (Var y' yt) y body
+    bin' = (y', yt)
+    y' = freshVar fvMN y
+    fvMN = Set.union (fv body) (fv n)
+-- TODO: I need to check let subsitution is correct
+subst n x l@(Let y e1 e2 t)
+    | y == x = l
+    | y `Set.notMember` fv n = Let y e1 (subst n x e2) t
+    | otherwise = Let y' e1 (subst n x e2') t
+  where
+    e2' = subst (Var y' (extract e1)) y e2'
+    y' = freshVar fvMN y
+    fvMN = Set.union (fv e2) (fv n)
+subst n x (Pair l r t) = Pair (subst n x l) (subst n x r) t
+subst n x (Fst e t) = Fst (subst n x e) t
+subst n x (Snd e t) = Fst (subst n x e) t
+subst _ _ t = t
+
+-- | Check if a term is normal
+normal :: LambdaTerm -> Bool
+normal ID{} = True
+normal UVar{} = True
+normal Var{} = True
+normal Abs{} = True -- call-by-value
+normal (Pair l r _) = normal l && normal r
+normal _ = False
+
+-- | Take a step towards being normalized
+step :: LambdaTerm -> LambdaTerm
+-- reduction rules
+step (App (Abs bin m _) n _) = subst n (fst bin) m
+step (Fst (Pair l _ _) _) = l
+step (Snd (Pair _ r _) _) = r
+step (Let v e1 e2 t)
+    | normal e1 = subst e1 v e2
+    | otherwise = Let v (step e1) e2 t
+step (App l r t)
+    | normal l = App l (step r) t
+    | otherwise = App (step l) r t
+step (Pair l r t)
+    | normal l = Pair l (step r) t
+    | otherwise = Pair (step l) r t
+step (Fst e t) = Fst (step e) t
+step (Snd e t) = Snd (step e) t
+step x = x
+
+-- | Normalize a term to the end
+normalize :: LambdaTerm -> LambdaTerm
+normalize t =
+    if normal t
+        then t
+        else normalize $ step t
